2da2f4aea0ff57b26e2125bec4c5690b27c69796
nfspeed007 <isr15001@byui.edu>
2021-01-07 09:26:15 -0700

    Add project files.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // step 2
+            string name = "Tyler Israelsen";
+            string location = "Chicago, IL";
+
+            // step 3
+            Console.WriteLine($"My name is {name}");
+            Console.WriteLine($"I am from {location}");
+
+            // step 4
+            DateTime currentDate = DateTime.Now;
+            Console.WriteLine($"The current date is { currentDate.ToString("d")}");
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
