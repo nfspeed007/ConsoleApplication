1eb7070402e61b78c1169329c0720c226165085d
nfspeed007 <isr15001@byui.edu>
2021-01-09 16:17:47 -0700

    W01 Assignment

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -20,7 +20,30 @@ namespace ConsoleApplication
 
             // step 4
             DateTime currentDate = DateTime.Now;
-            Console.WriteLine($"The current date is { currentDate.ToString("d")}");
+            Console.WriteLine($"The current date is {currentDate.ToString("d")}");
+
+            // step 5
+            DateTime christmas = new DateTime(2021, 12, 25);
+            double daysUntilChristmas = christmas.Subtract(DateTime.Today).TotalDays;
+            Console.WriteLine($"The number of days until Christmas is {daysUntilChristmas}");
+
+            // step 6
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+
+            Console.WriteLine("The length of the wood is " +
+                woodLength + " feet");
+            Console.WriteLine("The area of the glass is " +
+                glassArea + " square metres");
 
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
